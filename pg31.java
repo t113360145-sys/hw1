c2911f3264522f6d1fc57ea08ebbc44a0d838550
t113360145-sys <t113360145@ntut.org.tw>
2026-09-24 20:28:28 +0800

    first

---
+++ pg31.java
@@ -0,0 +1,18 @@
+public class pg31 {
+  public static void main(String[]args) {
+    System.out.println("1+2="+(1+2));
+    System.out.println("3*4="+(3*4));
+
+    int num1 = 2;
+    int num2 = 3;
+    int sum = num1 + num2;
+
+    System.out.println("變數num1的值是"+num1);
+    System.out.println("變數num2的值是"+num2);
+    System.out.println("num1+num2的值是"+sum);
+
+    num1 = num1 + 1;
+
+    System.out.println("變數num1的值+1是"+num1);
+  }
+}
